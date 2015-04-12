af61470cd742d827e6dc6b57011ebb1031259f48
tatgauthier <tatgauthier@gmail.com>
2015-04-12 15:06:31 -0500

    Automated tests and formatting

---
+++ Tests/automated tests.js
@@ -0,0 +1,26 @@
+<SCRIPT LANGUAGE="JavaScript">
+
+//all tests should be copied to javascipt console in chrome.
+function loginTest(){												//run from index.html
+	document.getElementById("email").value = "partykid@swbell.net";	//chosen because it is 
+																	//integral to the system
+	document.getElementById("password").value = "partykid";
+	document.getElementById("loginButton").click();
+}
+loginTest();
+
+
+//this test is special in that it will only work once, creating a user will make it so that
+//the same user cannot be added twice. the email must be changed for each test or the 
+//parse entry must be deleted each time this test is run. it was chosen to because it is an 
+//integral part of the system and it adds data to the database
+function registerTest(){
+	document.getElementById("registerButton").click();
+	document.getElementById("name").value = "Nick Renaldo";
+	document.getElementById("email").value = "nickyr@gmail.com";	
+	document.getElementById("address").value = "123 this street";
+	document.getElementById("1234567890").value = "1234567890";
+	document.getElementById("password").value = "partykid";
+	document.getElementById("signup-button").click();
+}
+registerTest();
